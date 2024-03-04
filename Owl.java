42ae690d670d45cdbf8688a8530437725e08329a
MipowGITHUB <mumininsodia@gmail.com>
2024-03-04 13:51:36 +0700

    first commit

---
+++ Owl.java
@@ -0,0 +1,22 @@
+public class Owl {
+    String name = "Proyek";
+    int age = 1;
+    String warna = "Cokelat";
+    String jenis = "Barred Eagle";
+    String makanan = "Reptile";
+
+    public void TampilkanOwl(){
+        System.out.println("Owl namanya : " + name);
+    }
+    public void makananOwl(){
+        System.out.println("Owl memakan : " + makanan);
+    }
+
+    public void UmurOwl(){
+        System.out.println("Owl berumur : " + age);
+    }
+    public void JenisOwl(){
+        System.out.println("Owl jenisnya : " + jenis);
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Owl owL = new Owl();
+        owL.TampilkanOwl();
+        owL.makananOwl();
+        owL.UmurOwl();
+        owL.JenisOwl();
+    }
+}
+
